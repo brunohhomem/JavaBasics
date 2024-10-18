7b4ccc25581172628a43d64bcc6501d97e4510f8
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-18 09:03:34 -0300

    first commit

---
+++ src/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 9;
+        int result = x + y;
+
+        System.out.println("The sum of X + Y is: " + result);
+
+    }
+}
